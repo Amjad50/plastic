beee5435db6a858b1e635018c7fc723e30fcd561
Amjad Alsharafi <amjadsharafi10@gmail.com>
2020-06-15 17:45:55 +0800

    6502: started building the CPU (2A03)
    - made the registers and status flags

---
+++ src/cpu/cpu6502.rs
@@ -0,0 +1,32 @@
+// flags: [N, V, _, B, D, I, Z, C]
+enum StatusFlag {
+    Carry = 1 << 0,
+    Zero = 1 << 1,
+    InterruptDisable = 1 << 2,
+    DecimalMode = 1 << 3,
+    BreakCommand = 1 << 4,
+    Overflow = 1 << 6,
+    Negative = 1 << 7,
+}
+
+pub struct CPU6502 {
+    reg_pc: u16,
+    reg_sp: u16,
+    reg_a: u8,
+    reg_x: u8,
+    reg_y: u8,
+    reg_status: u8,
+}
+
+impl CPU6502 {
+    pub fn new() -> Self {
+        CPU6502 {
+            reg_pc: 0,
+            reg_sp: 0,
+            reg_a: 0,
+            reg_x: 0,
+            reg_y: 0,
+            reg_status: 0,
+        }
+    }
+}
---
+++ src/cpu/mod.rs
@@ -0,0 +1,2 @@
+mod cpu6502;
+pub use cpu6502::CPU6502;
