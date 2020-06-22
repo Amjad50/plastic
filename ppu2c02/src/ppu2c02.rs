165e5b2216bafec97435823da9e5c270967371e6
Amjad Alsharafi <amjadsharafi10@gmail.com>
2020-06-22 11:47:39 +0800

    PPU: implemented background rendering (with scroll)
    - this is not tested yet
    - the cycles timing and scanline might need to be improved as well

--- ppu2c02/src/ppu2c02.rs
+++ ppu2c02/src/ppu2c02.rs
@@ -21,7 +21,10 @@ pub struct PPU2C02<'a, T: Bus> {
     x_scroll: u8,
     y_scroll: u8,
 
-    w_mode: bool,
+    w_toggle: bool, // this is used for registers that require 2 writes
+
+    bg_pattern_shift_registers: [u16; 2],
+    bg_palette_attribute_shift_registers: [u8; 2],
 
     bus: &'a mut T,
 }
@@ -50,7 +53,10 @@ where
             x_scroll: 0,
             y_scroll: 0,
 
-            w_mode: false,
+            w_toggle: false,
+
+            bg_pattern_shift_registers: [0; 2],
+            bg_palette_attribute_shift_registers: [0; 2],
 
             bus: bus,
         }
@@ -73,21 +79,33 @@ where
             Register::Control => {
                 self.reg_control = data;
                 self.nametable_selector = data & 0b11;
+
+                // update temp address
+                self.vram_address_tmp &= 0x3FF;
+                self.vram_address_tmp |= (self.nametable_selector as u16) << 10;
             }
             Register::Mask => self.reg_mask = data,
             Register::OmaAddress => self.reg_oma_addr = data,
             Register::OmaData => self.reg_oma_data = data,
             Register::Scroll => {
-                if self.w_mode {
+                if self.w_toggle {
                     self.x_scroll = data;
+
+                    // update temp address
+                    self.vram_address_tmp &= 0xFFFE; // lower 5 bits
+                    self.vram_address_tmp |= (data >> 3) as u16;
                 } else {
                     self.y_scroll = data;
+
+                    // update temp address
+                    self.vram_address_tmp &= 0xFC1F; // second 5 bits
+                    self.vram_address_tmp |= ((data as u16) << 3) & 0b11111000;
                 }
 
-                self.w_mode = !self.w_mode;
+                self.w_toggle = !self.w_toggle;
             }
             Register::PPUAddress => {
-                if self.w_mode {
+                if self.w_toggle {
                     // zero out the bottom 8 bits
                     self.vram_address_tmp &= 0xff00;
                     // set the data from the parameters
@@ -102,7 +120,7 @@ where
                     self.vram_address_cur = self.vram_address_tmp;
                 }
 
-                self.w_mode = !self.w_mode;
+                self.w_toggle = !self.w_toggle;
             }
             Register::PPUData => self.reg_ppu_data = data,
             Register::DmaOma => self.reg_oma_dma = data,
@@ -112,12 +130,100 @@ where
         };
     }
 
+    /*
+    ## PPU pattern table addressing ##
+    DCBA98 76543210
+    ---------------
+    0HRRRR CCCCPTTT
+    |||||| |||||+++- T: Fine Y offset, the row number within a tile
+    |||||| ||||+---- P: Bit plane (0: "lower"; 1: "upper")
+    |||||| ++++----- C: Tile column
+    ||++++---------- R: Tile row
+    |+-------------- H: Half of sprite table (0: "left"; 1: "right")
+    +--------------- 0: Pattern table is at $0000-$1FFF
+    */
+    fn fetch_pattern_background(&self, location: u8) -> [u8; 2] {
+        let fine_y = (self.y_scroll & 0b111) as u16;
+
+        // for background
+        let pattern_table_selector = if self.reg_control & 0x10 != 0 { 1 } else { 0 };
+
+        let low_plane_pattern = self
+            .bus
+            .read(pattern_table_selector << 12 | (location as u16) << 4 | 0 << 3 | fine_y);
+
+        let high_plane_pattern = self
+            .bus
+            .read(pattern_table_selector << 12 | (location as u16) << 4 | 1 << 3 | fine_y);
+
+        [low_plane_pattern, high_plane_pattern]
+    }
+
+    /*
+    ## Attribute address ##
+    NN 1111 YYY XXX
+    || |||| ||| +++-- high 3 bits of coarse X (x/4)
+    || |||| +++------ high 3 bits of coarse Y (y/4)
+    || ++++---------- attribute offset (960 bytes)
+    ++--------------- nametable select
+    */
+    fn fetch_attribute_byte(&self) -> u8 {
+        let x = (self.x_scroll >> 5) as u16;
+        let y = (self.y_scroll >> 5) as u16;
+
+        self.bus
+            .read((self.nametable_selector as u16) << 10 | 0xF << 6 | y << 3 | x)
+    }
+    /*
+    ## color location offset 0x3F00 ##
+    43210
+    |||||
+    |||++- Pixel value from tile data
+    |++--- Palette number from attribute table or OAM
+    +----- Background/Sprite select
+    */
+    fn get_pixel(&mut self) -> u8 {
+        let fine_x = self.x_scroll & 0b111;
+        let low_plane_bit = ((self.bg_pattern_shift_registers[0] >> fine_x as u16) & 0x1) as u8;
+        let high_plane_bit = ((self.bg_pattern_shift_registers[1] >> fine_x as u16) & 0x1) as u8;
+
+        let color_bit = high_plane_bit << 1 | low_plane_bit;
+
+        let current_attribute = self.bg_palette_attribute_shift_registers[0];
+        let attribute_location_x = (self.x_scroll >> 1) & 0xf;
+        let attribute_location_y = (self.y_scroll >> 1) & 0xf;
+
+        let attribute_location = attribute_location_y << 1 | attribute_location_x;
+
+        let palette = (current_attribute >> attribute_location) & 0b11;
+        let background = 1; // true
+
+        let color = self
+            .bus
+            .read(0x3F00 | (background << 4 | palette << 2 | color_bit << 2) as u16);
+
+        // advance the shift registers
+        for i in 0..=1 {
+            self.bg_pattern_shift_registers[i] >>= 1;
+        }
+
+        color
+    }
+
+    fn render_pixel(&mut self) {
+        let color = self.get_pixel();
+        // render this color
+    }
+
     // run one cycle, this should be fed from Master clock
     pub fn run_cycle(&mut self) {
         // current scanline
         match self.scanline {
             261 => {
                 // pre-render
+
+                // next round
+                self.scanline = 0;
             }
             0..=239 => {
                 // render
@@ -125,13 +231,24 @@ where
             }
             240 => {
                 // post-render
+                // idle
             }
             241..=260 => {
                 // vertical blanking
+                if self.cycle == 1 && self.scanline == 241 {
+                    self.reg_status |= 0x80; // set v-blank
+
+                    // FIXME: raise non-maskable interrupt to the CPU
+                }
             }
             _ => {
                 unreachable!();
             }
+        }
+        self.cycle += 1;
+        if self.cycle > 340 {
+            self.scanline += 1;
+            self.cycle = 0;
         }
     }
 
@@ -143,9 +260,75 @@ where
             }
             1..=256 => {
                 // main render
+                self.render_pixel();
+
+                // fetch and reload shift registers
+                if self.cycle % 8 == 0 {
+                    let nametable_tile = self.bus.read(self.vram_address_cur);
+                    let tile_pattern = self.fetch_pattern_background(nametable_tile);
+                    let attribute_byte = self.fetch_attribute_byte();
+
+                    // update th shift registers
+                    for i in 0..=1 {
+                        self.bg_pattern_shift_registers[i] &= 0xff;
+                        self.bg_pattern_shift_registers[i] |= (tile_pattern[i] as u16) << 8;
+                    }
+
+                    // reload attribute shift register
+                    // TODO: this does not seem like a shift register but not sure
+                    self.bg_palette_attribute_shift_registers[0] =
+                        self.bg_palette_attribute_shift_registers[1];
+                    self.bg_palette_attribute_shift_registers[1] = attribute_byte;
+
+                    // increment scrolling
+                    if self.cycle != 256 {
+                        self.x_scroll += 1;
+                        // increase X in vram current address
+                        self.vram_address_cur += 1;
+                    } else {
+                        self.y_scroll += 1;
+
+                        // update vram current address
+                        self.vram_address_cur &= 0xFC1F; // second 5 bits
+                        self.vram_address_cur |= ((self.y_scroll as u16) << 3) & 0b11111000;
+                    }
+                }
             }
-            257..=320 => {}
-            321..=340 => {}
+            257..=320 => {
+                if self.cycle == 257 {
+                    let fine_x = self.x_scroll & 0x7; // save
+                    self.x_scroll = ((self.vram_address_tmp & 0b11111) << 3) as u8;
+                    self.x_scroll |= fine_x; //restore
+                }
+            }
+            321..=340 => {
+                // lets just do it in the beginning
+                if self.cycle == 321 {
+                    // load next 2 bytes
+                    for _ in 0..2 {
+                        let nametable_tile = self.bus.read(self.vram_address_cur);
+                        let tile_pattern = self.fetch_pattern_background(nametable_tile);
+                        let attribute_byte = self.fetch_attribute_byte();
+
+                        // update th shift registers
+                        for i in 0..=1 {
+                            self.bg_pattern_shift_registers[i] >>= 8;
+                            self.bg_pattern_shift_registers[i] |= (tile_pattern[i] as u16) << 8;
+                        }
+
+                        // reload attribute shift register
+                        // TODO: this does not seem like a shift register but not sure
+                        self.bg_palette_attribute_shift_registers[0] =
+                            self.bg_palette_attribute_shift_registers[1];
+                        self.bg_palette_attribute_shift_registers[1] = attribute_byte;
+
+                        // increment scrolling
+                        self.x_scroll += 1;
+                        // increase X in vram current address
+                        self.vram_address_cur += 1;
+                    }
+                }
+            }
             _ => {
                 unreachable!();
             }
@@ -162,25 +345,5 @@ where
 
     tile address      = 0x2000 | (v & 0x0FFF)
     attribute address = 0x23C0 | (v & 0x0C00) | ((v >> 4) & 0x38) | ((v >> 2) & 0x07)
-
-
-    ## Attribute address ##
-    NN 1111 YYY XXX
-    || |||| ||| +++-- high 3 bits of coarse X (x/4)
-    || |||| +++------ high 3 bits of coarse Y (y/4)
-    || ++++---------- attribute offset (960 bytes)
-    ++--------------- nametable select
-
-
-    ## PPU pattern table addressing ##
-    DCBA98 76543210
-    ---------------
-    0HRRRR CCCCPTTT
-    |||||| |||||+++- T: Fine Y offset, the row number within a tile
-    |||||| ||||+---- P: Bit plane (0: "lower"; 1: "upper")
-    |||||| ++++----- C: Tile column
-    ||++++---------- R: Tile row
-    |+-------------- H: Half of sprite table (0: "left"; 1: "right")
-    +--------------- 0: Pattern table is at $0000-$1FFF
     */
 }
