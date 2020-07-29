4ba4a371880fd0462de1296666407281fcad1db2
Amjad Alsharafi <amjadsharafi10@gmail.com>
2020-07-29 12:25:52 +0800

    CPU: refactored `Instruction::from_byte` to use giant switch
    - switch is good since the compiler is using jump table, and because
      there are some bytes that do not have value, but later when we implement
      unofficial instructions it will be filled, then maybe we can convert it
      to an array

--- cpu6502/src/instruction.rs
+++ cpu6502/src/instruction.rs
@@ -110,154 +110,188 @@ impl AddressingMode {
 impl Instruction {
     // got this bit format from (http://nparker.llx.com/a2/opcodes.html)
     pub fn from_byte(byte: u8) -> Result<Instruction, ()> {
-        let cc = byte & 0b11;
-        let addressing_mode_bbb = (byte >> 2) & 0b111;
-        let opcode_aaa = (byte >> 5) & 0b111;
+        let (opcode, addressing_mode) = match byte {
+            0x05 => (Opcode::Ora, AddressingMode::ZeroPage),
+            0x15 => (Opcode::Ora, AddressingMode::ZeroPageIndexX),
+            0x0D => (Opcode::Ora, AddressingMode::Absolute),
+            0x1D => (Opcode::Ora, AddressingMode::AbsoluteX),
+            0x09 => (Opcode::Ora, AddressingMode::Immediate),
+            0x19 => (Opcode::Ora, AddressingMode::AbsoluteY),
+            0x01 => (Opcode::Ora, AddressingMode::XIndirect),
+            0x11 => (Opcode::Ora, AddressingMode::IndirectY),
 
-        let (opcode, addressing_mode) = match cc {
-            0b01 => {
-                let opcode = match opcode_aaa {
-                    0b000 => Opcode::Ora,
-                    0b001 => Opcode::And,
-                    0b010 => Opcode::Eor,
-                    0b011 => Opcode::Adc,
-                    0b100 => Opcode::Sta,
-                    0b101 => Opcode::Lda,
-                    0b110 => Opcode::Cmp,
-                    0b111 => Opcode::Sbc,
-                    _ => unreachable!(),
-                };
-                let addressing_mode = match addressing_mode_bbb {
-                    0b000 => AddressingMode::XIndirect,
-                    0b001 => AddressingMode::ZeroPage,
-                    0b010 => AddressingMode::Immediate,
-                    0b011 => AddressingMode::Absolute,
-                    0b100 => AddressingMode::IndirectY,
-                    0b101 => AddressingMode::ZeroPageIndexX,
-                    0b110 => AddressingMode::AbsoluteY,
-                    0b111 => AddressingMode::AbsoluteX,
-                    _ => unreachable!(),
-                };
+            0x25 => (Opcode::And, AddressingMode::ZeroPage),
+            0x35 => (Opcode::And, AddressingMode::ZeroPageIndexX),
+            0x2D => (Opcode::And, AddressingMode::Absolute),
+            0x3D => (Opcode::And, AddressingMode::AbsoluteX),
+            0x29 => (Opcode::And, AddressingMode::Immediate),
+            0x39 => (Opcode::And, AddressingMode::AbsoluteY),
+            0x21 => (Opcode::And, AddressingMode::XIndirect),
+            0x31 => (Opcode::And, AddressingMode::IndirectY),
 
-                // This instruction does not exists (STA with immediate)
-                if opcode == Opcode::Sta && addressing_mode == AddressingMode::Immediate {
-                    return Err(());
-                }
+            0x45 => (Opcode::Eor, AddressingMode::ZeroPage),
+            0x55 => (Opcode::Eor, AddressingMode::ZeroPageIndexX),
+            0x4D => (Opcode::Eor, AddressingMode::Absolute),
+            0x5D => (Opcode::Eor, AddressingMode::AbsoluteX),
+            0x49 => (Opcode::Eor, AddressingMode::Immediate),
+            0x59 => (Opcode::Eor, AddressingMode::AbsoluteY),
+            0x41 => (Opcode::Eor, AddressingMode::XIndirect),
+            0x51 => (Opcode::Eor, AddressingMode::IndirectY),
 
-                (opcode, addressing_mode)
+            0x65 => (Opcode::Adc, AddressingMode::ZeroPage),
+            0x75 => (Opcode::Adc, AddressingMode::ZeroPageIndexX),
+            0x6D => (Opcode::Adc, AddressingMode::Absolute),
+            0x7D => (Opcode::Adc, AddressingMode::AbsoluteX),
+            0x69 => (Opcode::Adc, AddressingMode::Immediate),
+            0x79 => (Opcode::Adc, AddressingMode::AbsoluteY),
+            0x61 => (Opcode::Adc, AddressingMode::XIndirect),
+            0x71 => (Opcode::Adc, AddressingMode::IndirectY),
+
+            0x85 => (Opcode::Sta, AddressingMode::ZeroPage),
+            0x95 => (Opcode::Sta, AddressingMode::ZeroPageIndexX),
+            0x8D => (Opcode::Sta, AddressingMode::Absolute),
+            0x9D => (Opcode::Sta, AddressingMode::AbsoluteX),
+            0x99 => (Opcode::Sta, AddressingMode::AbsoluteY),
+            0x81 => (Opcode::Sta, AddressingMode::XIndirect),
+            0x91 => (Opcode::Sta, AddressingMode::IndirectY),
+
+            0xA5 => (Opcode::Lda, AddressingMode::ZeroPage),
+            0xB5 => (Opcode::Lda, AddressingMode::ZeroPageIndexX),
+            0xAD => (Opcode::Lda, AddressingMode::Absolute),
+            0xBD => (Opcode::Lda, AddressingMode::AbsoluteX),
+            0xA9 => (Opcode::Lda, AddressingMode::Immediate),
+            0xB9 => (Opcode::Lda, AddressingMode::AbsoluteY),
+            0xA1 => (Opcode::Lda, AddressingMode::XIndirect),
+            0xB1 => (Opcode::Lda, AddressingMode::IndirectY),
+
+            0xC5 => (Opcode::Cmp, AddressingMode::ZeroPage),
+            0xD5 => (Opcode::Cmp, AddressingMode::ZeroPageIndexX),
+            0xCD => (Opcode::Cmp, AddressingMode::Absolute),
+            0xDD => (Opcode::Cmp, AddressingMode::AbsoluteX),
+            0xC9 => (Opcode::Cmp, AddressingMode::Immediate),
+            0xD9 => (Opcode::Cmp, AddressingMode::AbsoluteY),
+            0xC1 => (Opcode::Cmp, AddressingMode::XIndirect),
+            0xD1 => (Opcode::Cmp, AddressingMode::IndirectY),
+
+            0xE5 => (Opcode::Sbc, AddressingMode::ZeroPage),
+            0xF5 => (Opcode::Sbc, AddressingMode::ZeroPageIndexX),
+            0xED => (Opcode::Sbc, AddressingMode::Absolute),
+            0xFD => (Opcode::Sbc, AddressingMode::AbsoluteX),
+            0xE9 => (Opcode::Sbc, AddressingMode::Immediate),
+            0xF9 => (Opcode::Sbc, AddressingMode::AbsoluteY),
+            0xE1 => (Opcode::Sbc, AddressingMode::XIndirect),
+            0xF1 => (Opcode::Sbc, AddressingMode::IndirectY),
+
+            0x0A => (Opcode::Asl, AddressingMode::Accumulator),
+            0x06 => (Opcode::Asl, AddressingMode::ZeroPage),
+            0x16 => (Opcode::Asl, AddressingMode::ZeroPageIndexX),
+            0x0E => (Opcode::Asl, AddressingMode::Absolute),
+            0x1E => (Opcode::Asl, AddressingMode::AbsoluteX),
+
+            0x2A => (Opcode::Rol, AddressingMode::Accumulator),
+            0x26 => (Opcode::Rol, AddressingMode::ZeroPage),
+            0x36 => (Opcode::Rol, AddressingMode::ZeroPageIndexX),
+            0x2E => (Opcode::Rol, AddressingMode::Absolute),
+            0x3E => (Opcode::Rol, AddressingMode::AbsoluteX),
+
+            0x4A => (Opcode::Lsr, AddressingMode::Accumulator),
+            0x46 => (Opcode::Lsr, AddressingMode::ZeroPage),
+            0x56 => (Opcode::Lsr, AddressingMode::ZeroPageIndexX),
+            0x4E => (Opcode::Lsr, AddressingMode::Absolute),
+            0x5E => (Opcode::Lsr, AddressingMode::AbsoluteX),
+
+            0x6A => (Opcode::Ror, AddressingMode::Accumulator),
+            0x66 => (Opcode::Ror, AddressingMode::ZeroPage),
+            0x76 => (Opcode::Ror, AddressingMode::ZeroPageIndexX),
+            0x6E => (Opcode::Ror, AddressingMode::Absolute),
+            0x7E => (Opcode::Ror, AddressingMode::AbsoluteX),
+
+            0x86 => (Opcode::Stx, AddressingMode::ZeroPage),
+            0x96 => (Opcode::Stx, AddressingMode::ZeroPageIndexY),
+            0x8E => (Opcode::Stx, AddressingMode::Absolute),
+
+            0xA2 => (Opcode::Ldx, AddressingMode::Immediate),
+            0xA6 => (Opcode::Ldx, AddressingMode::ZeroPage),
+            0xB6 => (Opcode::Ldx, AddressingMode::ZeroPageIndexY),
+            0xAE => (Opcode::Ldx, AddressingMode::Absolute),
+            0xBE => (Opcode::Ldx, AddressingMode::AbsoluteY),
+
+            0xC6 => (Opcode::Dec, AddressingMode::ZeroPage),
+            0xD6 => (Opcode::Dec, AddressingMode::ZeroPageIndexX),
+            0xCE => (Opcode::Dec, AddressingMode::Absolute),
+            0xDE => (Opcode::Dec, AddressingMode::AbsoluteX),
+
+            0xE6 => (Opcode::Inc, AddressingMode::ZeroPage),
+            0xF6 => (Opcode::Inc, AddressingMode::ZeroPageIndexX),
+            0xEE => (Opcode::Inc, AddressingMode::Absolute),
+            0xFE => (Opcode::Inc, AddressingMode::AbsoluteX),
+
+            0x24 => (Opcode::Bit, AddressingMode::ZeroPage),
+            0x2C => (Opcode::Bit, AddressingMode::Absolute),
+
+            0x4C => (Opcode::Jmp, AddressingMode::Absolute),
+            0x6C => (Opcode::Jmp, AddressingMode::Indirect),
+
+            0x84 => (Opcode::Sty, AddressingMode::ZeroPage),
+            0x94 => (Opcode::Sty, AddressingMode::ZeroPageIndexX),
+            0x8C => (Opcode::Sty, AddressingMode::Absolute),
+
+            0xA0 => (Opcode::Ldy, AddressingMode::Immediate),
+            0xA4 => (Opcode::Ldy, AddressingMode::ZeroPage),
+            0xB4 => (Opcode::Ldy, AddressingMode::ZeroPageIndexX),
+            0xAC => (Opcode::Ldy, AddressingMode::Absolute),
+            0xBC => (Opcode::Ldy, AddressingMode::AbsoluteX),
+
+            0xC0 => (Opcode::Cpy, AddressingMode::Immediate),
+            0xC4 => (Opcode::Cpy, AddressingMode::ZeroPage),
+            0xCC => (Opcode::Cpy, AddressingMode::Absolute),
+
+            0xE0 => (Opcode::Cpx, AddressingMode::Immediate),
+            0xE4 => (Opcode::Cpx, AddressingMode::ZeroPage),
+            0xEC => (Opcode::Cpx, AddressingMode::Absolute),
+
+            0x10 => (Opcode::Bpl, AddressingMode::Relative),
+            0x30 => (Opcode::Bmi, AddressingMode::Relative),
+            0x50 => (Opcode::Bvc, AddressingMode::Relative),
+            0x70 => (Opcode::Bvs, AddressingMode::Relative),
+            0x90 => (Opcode::Bcc, AddressingMode::Relative),
+            0xB0 => (Opcode::Bcs, AddressingMode::Relative),
+            0xD0 => (Opcode::Bne, AddressingMode::Relative),
+            0xF0 => (Opcode::Beq, AddressingMode::Relative),
+
+            0x00 => (Opcode::Brk, AddressingMode::Implied),
+            0x20 => (Opcode::Jsr, AddressingMode::Absolute),
+            0x40 => (Opcode::Rti, AddressingMode::Implied),
+            0x60 => (Opcode::Rts, AddressingMode::Implied),
+
+            0x08 => (Opcode::Php, AddressingMode::Implied),
+            0x28 => (Opcode::Plp, AddressingMode::Implied),
+            0x48 => (Opcode::Pha, AddressingMode::Implied),
+            0x68 => (Opcode::Pla, AddressingMode::Implied),
+            0x88 => (Opcode::Dey, AddressingMode::Implied),
+            0xA8 => (Opcode::Tay, AddressingMode::Implied),
+            0xC8 => (Opcode::Iny, AddressingMode::Implied),
+            0xE8 => (Opcode::Inx, AddressingMode::Implied),
+
+            0x18 => (Opcode::Clc, AddressingMode::Implied),
+            0x38 => (Opcode::Sec, AddressingMode::Implied),
+            0x58 => (Opcode::Cli, AddressingMode::Implied),
+            0x78 => (Opcode::Sei, AddressingMode::Implied),
+            0x98 => (Opcode::Tya, AddressingMode::Implied),
+            0xB8 => (Opcode::Clv, AddressingMode::Implied),
+            0xD8 => (Opcode::Cld, AddressingMode::Implied),
+            0xF8 => (Opcode::Sed, AddressingMode::Implied),
+
+            0x8A => (Opcode::Txa, AddressingMode::Implied),
+            0x9A => (Opcode::Txs, AddressingMode::Implied),
+            0xAA => (Opcode::Tax, AddressingMode::Implied),
+            0xBA => (Opcode::Tsx, AddressingMode::Implied),
+            0xCA => (Opcode::Dex, AddressingMode::Implied),
+            0xEA => (Opcode::Nop, AddressingMode::Implied),
+
+            _ => {
+                return Err(());
             }
-            _ => match byte {
-                0x0A => (Opcode::Asl, AddressingMode::Accumulator),
-                0x06 => (Opcode::Asl, AddressingMode::ZeroPage),
-                0x16 => (Opcode::Asl, AddressingMode::ZeroPageIndexX),
-                0x0E => (Opcode::Asl, AddressingMode::Absolute),
-                0x1E => (Opcode::Asl, AddressingMode::AbsoluteX),
-
-                0x2A => (Opcode::Rol, AddressingMode::Accumulator),
-                0x26 => (Opcode::Rol, AddressingMode::ZeroPage),
-                0x36 => (Opcode::Rol, AddressingMode::ZeroPageIndexX),
-                0x2E => (Opcode::Rol, AddressingMode::Absolute),
-                0x3E => (Opcode::Rol, AddressingMode::AbsoluteX),
-
-                0x4A => (Opcode::Lsr, AddressingMode::Accumulator),
-                0x46 => (Opcode::Lsr, AddressingMode::ZeroPage),
-                0x56 => (Opcode::Lsr, AddressingMode::ZeroPageIndexX),
-                0x4E => (Opcode::Lsr, AddressingMode::Absolute),
-                0x5E => (Opcode::Lsr, AddressingMode::AbsoluteX),
-
-                0x6A => (Opcode::Ror, AddressingMode::Accumulator),
-                0x66 => (Opcode::Ror, AddressingMode::ZeroPage),
-                0x76 => (Opcode::Ror, AddressingMode::ZeroPageIndexX),
-                0x6E => (Opcode::Ror, AddressingMode::Absolute),
-                0x7E => (Opcode::Ror, AddressingMode::AbsoluteX),
-
-                0x86 => (Opcode::Stx, AddressingMode::ZeroPage),
-                0x96 => (Opcode::Stx, AddressingMode::ZeroPageIndexY),
-                0x8E => (Opcode::Stx, AddressingMode::Absolute),
-
-                0xA2 => (Opcode::Ldx, AddressingMode::Immediate),
-                0xA6 => (Opcode::Ldx, AddressingMode::ZeroPage),
-                0xB6 => (Opcode::Ldx, AddressingMode::ZeroPageIndexY),
-                0xAE => (Opcode::Ldx, AddressingMode::Absolute),
-                0xBE => (Opcode::Ldx, AddressingMode::AbsoluteY),
-
-                0xC6 => (Opcode::Dec, AddressingMode::ZeroPage),
-                0xD6 => (Opcode::Dec, AddressingMode::ZeroPageIndexX),
-                0xCE => (Opcode::Dec, AddressingMode::Absolute),
-                0xDE => (Opcode::Dec, AddressingMode::AbsoluteX),
-
-                0xE6 => (Opcode::Inc, AddressingMode::ZeroPage),
-                0xF6 => (Opcode::Inc, AddressingMode::ZeroPageIndexX),
-                0xEE => (Opcode::Inc, AddressingMode::Absolute),
-                0xFE => (Opcode::Inc, AddressingMode::AbsoluteX),
-
-                0x24 => (Opcode::Bit, AddressingMode::ZeroPage),
-                0x2C => (Opcode::Bit, AddressingMode::Absolute),
-
-                0x4C => (Opcode::Jmp, AddressingMode::Absolute),
-                0x6C => (Opcode::Jmp, AddressingMode::Indirect),
-
-                0x84 => (Opcode::Sty, AddressingMode::ZeroPage),
-                0x94 => (Opcode::Sty, AddressingMode::ZeroPageIndexX),
-                0x8C => (Opcode::Sty, AddressingMode::Absolute),
-
-                0xA0 => (Opcode::Ldy, AddressingMode::Immediate),
-                0xA4 => (Opcode::Ldy, AddressingMode::ZeroPage),
-                0xB4 => (Opcode::Ldy, AddressingMode::ZeroPageIndexX),
-                0xAC => (Opcode::Ldy, AddressingMode::Absolute),
-                0xBC => (Opcode::Ldy, AddressingMode::AbsoluteX),
-
-                0xC0 => (Opcode::Cpy, AddressingMode::Immediate),
-                0xC4 => (Opcode::Cpy, AddressingMode::ZeroPage),
-                0xCC => (Opcode::Cpy, AddressingMode::Absolute),
-
-                0xE0 => (Opcode::Cpx, AddressingMode::Immediate),
-                0xE4 => (Opcode::Cpx, AddressingMode::ZeroPage),
-                0xEC => (Opcode::Cpx, AddressingMode::Absolute),
-
-                0x10 => (Opcode::Bpl, AddressingMode::Relative),
-                0x30 => (Opcode::Bmi, AddressingMode::Relative),
-                0x50 => (Opcode::Bvc, AddressingMode::Relative),
-                0x70 => (Opcode::Bvs, AddressingMode::Relative),
-                0x90 => (Opcode::Bcc, AddressingMode::Relative),
-                0xB0 => (Opcode::Bcs, AddressingMode::Relative),
-                0xD0 => (Opcode::Bne, AddressingMode::Relative),
-                0xF0 => (Opcode::Beq, AddressingMode::Relative),
-
-                0x00 => (Opcode::Brk, AddressingMode::Implied),
-                0x20 => (Opcode::Jsr, AddressingMode::Absolute),
-                0x40 => (Opcode::Rti, AddressingMode::Implied),
-                0x60 => (Opcode::Rts, AddressingMode::Implied),
-
-                0x08 => (Opcode::Php, AddressingMode::Implied),
-                0x28 => (Opcode::Plp, AddressingMode::Implied),
-                0x48 => (Opcode::Pha, AddressingMode::Implied),
-                0x68 => (Opcode::Pla, AddressingMode::Implied),
-                0x88 => (Opcode::Dey, AddressingMode::Implied),
-                0xA8 => (Opcode::Tay, AddressingMode::Implied),
-                0xC8 => (Opcode::Iny, AddressingMode::Implied),
-                0xE8 => (Opcode::Inx, AddressingMode::Implied),
-
-                0x18 => (Opcode::Clc, AddressingMode::Implied),
-                0x38 => (Opcode::Sec, AddressingMode::Implied),
-                0x58 => (Opcode::Cli, AddressingMode::Implied),
-                0x78 => (Opcode::Sei, AddressingMode::Implied),
-                0x98 => (Opcode::Tya, AddressingMode::Implied),
-                0xB8 => (Opcode::Clv, AddressingMode::Implied),
-                0xD8 => (Opcode::Cld, AddressingMode::Implied),
-                0xF8 => (Opcode::Sed, AddressingMode::Implied),
-
-                0x8A => (Opcode::Txa, AddressingMode::Implied),
-                0x9A => (Opcode::Txs, AddressingMode::Implied),
-                0xAA => (Opcode::Tax, AddressingMode::Implied),
-                0xBA => (Opcode::Tsx, AddressingMode::Implied),
-                0xCA => (Opcode::Dex, AddressingMode::Implied),
-                0xEA => (Opcode::Nop, AddressingMode::Implied),
-
-                _ => {
-                    return Err(());
-                }
-            },
         };
 
         Ok(Instruction {
